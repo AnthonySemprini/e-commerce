8f1569208d69522b52d9544bc2ef1172d46e1b66
Anthony <semprini.anthony@gmail.com>
2024-03-05 16:21:14 +0100

    liste des produit

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProduitRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProduitController extends AbstractController
+{
+    #[Route('/produit', name: 'app_produit')]
+    public function index(ProduitRepository $produitRepository): Response
+    {
+        $produits = $produitRepository->findAll();
+
+        return $this->render('produit/index.html.twig', [
+            'produits' => $produits,
+        ]);
+    }
+}
